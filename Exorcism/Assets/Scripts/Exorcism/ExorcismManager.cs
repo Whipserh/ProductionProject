95080c510f8f71c16a6d686d7071493831d01850
Whipserh <mcbrider@sheridancollege.ca>
2025-02-12 20:53:42 -0500

    Exorcism -part working

---
+++ Exorcism/Assets/Scripts/Exorcism/ExorcismManager.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Xml.Schema;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ExorcismManager : MonoBehaviour
+{
+
+    public static float progression = 0;
+    public float decreasingRate = 1;
+    public float spawnRate = 1;
+    public float[] percentageRate;
+    public List<GameObject> sigilPrefabs;// = new List<GameObject>();
+    private float timer;
+    public GameObject pocession;  
+    public Transform sigilTransforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        //double check the chances for each sigal
+       float totalChance = 0;
+       foreach(float chance in percentageRate)
+       {
+            totalChance += chance;
+       }
+       
+       if(totalChance > 1)
+       {
+            Debug.LogError("Total percentage does not equal to 100%");
+       }
+
+       //set the exorcism 
+       resetExorcism();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spawn a new sigil every so often
+        timer += Time.deltaTime;
+        if(timer >= spawnRate)
+        {
+            spawnSigil();
+        }
+
+
+        //successful able to exorcism them
+        if(progression >= 100)
+        {
+
+        }
+    }
+
+
+    public float width, length;
+    //TO DO
+    public void spawnSigil()
+    {
+        //choose a sigil to spawn
+        float choice = Random.Range(0, 1f);
+        int index = 0;
+        float total = 0;
+        for (index = 0; index < percentageRate.Length; index++)
+        { total += percentageRate[index];
+            if(choice <= total)
+            {
+                break;//we exit the loop if our choice falls into the percentage range
+            }
+        }
+
+        //chose spawn location and direction
+        float x;
+        if (Random.Range(0, 1) == 0)
+        {
+            x = transform.position.x - length / 2;
+        }
+        else
+        {
+            x = transform.position.x + length / 2;
+        }
+        Vector2 position = new Vector2(x, Random.Range(transform.position.y - (width/2), transform.position.y + (width / 2)));
+        
+
+        //create the object
+        GameObject sigil = Instantiate(sigilPrefabs[index], position, transform.rotation, sigilTransforms);
+        SigalMovement sigilMove = sigil.GetComponent<SigalMovement>();
+        sigilMove.exorcismManager = gameObject;
+        sigilMove.length = length;
+        sigilMove.width = width;
+
+    }//end spawn sigil
+
+
+    //DONE - to be tested
+    public void resetExorcism()
+    {
+        //reset the time
+        timer = 0;
+
+        //restart the progression
+        progression = 0;
+
+        //make the crewmate possessed
+        setPossessedSprite(true);
+        
+
+    }//reset exorcism
+
+    //DONE-- TO BE TESTED
+    public void setPossessedSprite(bool isPossessed)
+    {
+
+        //----------------------------------------------------find the body of the crewmate
+        GameObject body = null;
+        //search for possession in the child
+        for (int i = 0; i < pocession.transform.childCount; i++)
+        {
+            body = pocession.transform.GetChild(i).gameObject;
+            if (body.name == "body")
+            {
+                break;//if we found the pocessed body then we break from the loop
+            }
+
+        }
+
+        //------------------------------------------------find the related sprites and disable them
+        //search the body assets
+        if (body != null)
+            for (int i = 0; i < body.transform.childCount; i++)
+            {
+
+                GameObject part = body.transform.GetChild(i).gameObject;
+
+
+                if (part.name == "possessed")//if this is the body is the possessed sprite active at it (if true)
+                {
+                    part.SetActive(isPossessed);
+                    continue;
+                }
+                else if (part.name == "normal")//if this is the normal sprite deactivate it (if true)
+                {
+                    part.SetActive(!isPossessed);
+                    continue;
+                }
+            }
+    }//end setPossessed
+}
---
+++ Exorcism/Assets/Scripts/Exorcism/SigalMovement.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
+
+public class SigalMovement : MonoBehaviour
+{
+    public GameObject exorcismManager;
+    public int movementType;
+    public float speed;
+    public float turnSpeed;
+    public float acceleration;
+
+    public float length, width;
+
+
+
+    private Vector2 goal;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //create the goal location based on the type of movement
+        float x, y;
+        switch (movementType) {
+            
+            case 1://bounceBack to the same side that it came back on
+                //go to the same side that it had enter on
+                x = transform.position.x;
+                
+                //choose a random point on the oposite side that it came from
+                if (transform.position.y < exorcismManager.transform.position.y)//if the sigil came from the bottom send it to the top
+                {
+                    y = Random.Range(exorcismManager.transform.position.y, exorcismManager.transform.position.y + width / 2);
+                }
+                else//if top then bottom
+                {
+                    y = Random.Range(exorcismManager.transform.position.y - width / 2, exorcismManager.transform.position.y);
+                }
+
+                goal = new Vector2(x, y);
+                goalTransform.position = goal;
+                break;
+
+            default://goes Across the scene either movement 0 or 2
+                if (transform.position.x < exorcismManager.transform.position.x)//sigil is going from left to right
+                {
+                    x = exorcismManager.transform.position.x + length / 2;
+                }
+                else//sigil is going from right to left
+                {
+                    x = exorcismManager.transform.position.x - length / 2;
+                }
+                y = Random.Range(exorcismManager.transform.position.y - width / 2, exorcismManager.transform.position.y + width / 2);
+                goal = new Vector2(x, y);
+                goalTransform.position = goal;
+                break;
+        }
+
+        Debug.Log(goal);
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 10);
+    }
+
+    private float timer = 0;
+    public Transform goalTransform;
+    public float amptitude = 1, period = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        switch (movementType)
+        {
+
+
+            case 2://sin wave
+                Vector2 direction = exorcismManager.transform.position - transform.position;
+                
+                float angle = Mathf.Atan2(direction.y, direction.x );
+                Debug.Log(angle);
+                Vector2 sinwave = amptitude * Mathf.Sin((360/period)*timer * Mathf.Deg2Rad) * (new Vector2(Mathf.Abs(Mathf.Sin(angle)), Mathf.Abs(Mathf.Cos(angle))));
+                Debug.Log(sinwave);
+                transform.position += (Vector3)sinwave + (transform.right*Time.deltaTime);
+                break;
+            default: //straight/simi-linear movement
+                 turn(goal - (Vector2)transform.position);
+                rb.velocity = transform.right * speed;
+                break;
+        }
+    }
+
+
+    public float turnDebuffer = 1f;
+    private void turn(Vector3 direction)
+    {
+
+        float currentAngle = (Mathf.Atan2(transform.up.y, transform.up.x) * Mathf.Rad2Deg);
+        float targetAngle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+        //Debug.Log(currentAngle + ", " + targetAngle);
+        if (currentAngle < 0)
+            currentAngle += 360;
+        if (targetAngle < 0)
+            targetAngle += 360;
+        Debug.Log(currentAngle + ", " + targetAngle);
+        //Debug.Log(Mathf.FloorToInt(180 + currentAngle) % 360);
+        //if we are not in our buffer range then we move
+        if (Mathf.Abs(currentAngle - targetAngle) > turnDebuffer)
+        {
+
+            if (targetAngle < currentAngle)
+            {
+                if (Mathf.Abs(currentAngle - targetAngle) < 180)
+                {
+                    transform.Rotate(new Vector3(0, 0, -1 * turnSpeed * Time.deltaTime));
+                }
+                else
+                {
+                    transform.Rotate(new Vector3(0, 0, turnSpeed * Time.deltaTime));//turn counterclockwise
+                }
+            }
+            else
+            {
+                if (Mathf.Abs(currentAngle - targetAngle) < 180)
+                {
+                    transform.Rotate(new Vector3(0, 0, turnSpeed * Time.deltaTime));//turn counterclockwise
+
+                }
+                else
+                {
+                    transform.Rotate(new Vector3(0, 0, -1 * turnSpeed * Time.deltaTime));
+                }
+
+            }
+
+
+        }
+
+    }//end turn function
+
+    //when the player clicks on the mouse
+    private void OnMouseDown()
+    {
+        //TODO:play animation
+
+        //update the progression
+
+        //destroy object
+        Destroy(gameObject);
+    }
+
+}

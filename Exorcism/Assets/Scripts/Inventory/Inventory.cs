6f4b5f45fe250a02196bf9570cecfdc2aac39de4
Whipserh <mcbrider@sheridancollege.ca>
2025-03-12 23:34:02 -0400

    progress

--- Exorcism/Assets/Scripts/Inventory/Inventory.cs
+++ Exorcism/Assets/Scripts/Inventory/Inventory.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Inventory : MonoBehaviour
 {
@@ -10,19 +11,9 @@ public class Inventory : MonoBehaviour
     public GameObject[] images = new GameObject[4];  
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
 
+    //sets all the items to bools
     public void unequip()
     {
         for(int i = 0; i < equiped.Length; i++)
@@ -33,22 +24,40 @@ public class Inventory : MonoBehaviour
 
     public void selectedItem1()
     {
-        
+        equipItem(0);
     }
     public void selectedItem2()
     {
-
+        equipItem(1);
     }
     public void selectedItem3()
     {
-
-
+        equipItem(2);
     }
     public void selectedItem4()
     {
-
+        equipItem(3);
     }
 
+    //takes an inventory index and activates it
+    public void equipItem(int index)
+    {
+        unequip();
+        if (hasInInventory[index])
+        {
+            equiped[index] = true;
+            Debug.Log($"Player equiped {images[index].name}");
+        }
+    }
 
+    //returns the string that the player has equiped
+    public string getEquipedItem()
+    {
+        for(int i = 0; i < hasInInventory.Length; i++)
+        {
+            if (hasInInventory[i] && equiped[i]) return images[i].GetComponent<UnityEngine.UI.Image>().sprite.name;
+        }
+        return "";
+    }
 
 }
